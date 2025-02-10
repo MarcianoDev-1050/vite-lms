5ce7a9592f1c4743c49070eacf5eaa11a9f5386a
MarcianoDev-1050 <aesml1996@gmail.com>
2025-02-10 16:33:23 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-lms/', // Asegúrate de usar el nombre correcto de tu repositorio
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'dist', // Asegura que el build se genere en "dist"
   },
 });
